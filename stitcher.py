69331c67d242f0faa93daec5c1fe9778726654c2
lenart12 <koslenart@gmail.com>
2020-03-30 03:20:21 +0200

    Changed font rendering order

--- stitcher.py
+++ stitcher.py
@@ -52,11 +52,13 @@ def make_wti_interpolater(in_min, in_max, out_min, out_max):
 
 # Function to draw text so that x,y is in the middle of the draw text
 def centered_text(draw, font, text, x, y):
-    size = draw.textsize(text, font, stroke_width=5)
+    size = draw.textsize(text, font, stroke_width=2)
     n_x = x - size[0]/2
     n_y = y - size[1]/2
-    draw.text((n_x + 5, n_y + 5), font=font, text=text, fill=(0, 0, 0), stroke_fill=(0, 0, 0), stroke_width=5)
-    draw.text((n_x, n_y), font=font, text=text, fill=(255, 255, 255))
+    # Draw shadow
+    draw.text((n_x + 5, n_y + 5), font=font, text=text, fill=(0, 0, 0), stroke_fill=(0, 0, 0), stroke_width=7)
+    # Draw text
+    draw.text((n_x, n_y), font=font, text=text, fill=(255, 255, 255), stroke_fill=(255, 255, 255), stroke_width=2)
 
 def main():
     # Create target image
@@ -94,6 +96,7 @@ def main():
         )
 
     if DRAW_CAPTURE_ZONE:
+        # Create drawing object
         zones_overlay = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 0))
         zones_draw = ImageDraw.Draw(zones_overlay)
         
@@ -165,7 +168,7 @@ def main():
                 n_y = wti_itp_y(next_flag_loc['y'])
                 draw.line( [x, y, n_x, n_y], fill=(255, 255, 255, 169), width=15)
 
-        # Draw flags and name
+        # Draw flags
         for _, flag in flag_data['flags'].items():
             print(f'Drawing flag for {flag["display_name"]}')
             x = wti_itp_x(flag['location']['x'])
@@ -176,14 +179,20 @@ def main():
                 (int(x - team_flags[int(flag['initial_team'])].size[0]/2),
                  int(y - team_flags[int(flag['initial_team'])].size[1]/2))
             )
-            # Draw objective name
-            centered_text(draw, font, flag['display_name'], x, y - 200)
 
         print('Compositing objectives to image')
 
         transparency = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 55))
         objectives_overlay = ImageChops.subtract(objectives_overlay, transparency)
-        minimap.alpha_composite(objectives_overlay)     
+        minimap.alpha_composite(objectives_overlay)   
+
+        draw = ImageDraw.Draw(minimap)
+        # Draw name of flag
+        for _, flag in flag_data['flags'].items():
+            print(f'Drawing name for {flag["display_name"]}')
+            x = wti_itp_x(flag['location']['x'])
+            y = wti_itp_y(flag['location']['y'])
+            centered_text(draw, font, flag['display_name'], x, y - 230)  
 
     if DRAW_BORDER:
         print('Parsing border line')
