f23585e05c70419922aa33aea30d3db0d92de53b
lenart12 <koslenart@gmail.com>
2020-03-30 02:42:05 +0200

    Fix zone rendring border exporting/rendring output to file

--- readme.md
+++ readme.md
@@ -1,14 +1,47 @@
 Squad High Resolution Map Extractor
 ======
-Note: cap zone rendring not working properly
-###
 
-![Preview](https://github.com/Lenart12/SquadHighResolutionMapExtractor/blob/master/Sumari_AAS_v1_stitched.jpg)
+
+Comparison with default
+####
+
+New:
+![Preview](Sumari_AAS_v1_stitched.jpg)
+
+Vanilla:
+![Vanilla](https://squadmaps.com/full/Sumari_AAS_v1.jpg)
+
+---
 
 Requirements
  * [Squad SDK](https://squad.gamepedia.com/Squad_SDK#Downloading_the_Epic_Games_Launcher)
  * [Python 3.6+](https://www.python.org/)
     * [PIL](https://pypi.org/project/Pillow/) (pip install pillow)
+
+
+---
+Usage
+1. Open up SDK, and open up a layer (_don't open master layer unless you know how to add lighting_) you want to render (_first time it will take a very long time_).
+
+2. Open world properties and find minimap generation tool. Ingame minimaps have a resolution 4096 with a tile of 1. Increasing tiles will increase final resolution, however it might use up too much ram and crash if you choose a too large of a resolution-tile combo.
+
+   __IMPORTANT!__ Before generating minimap tiles execute this command - `r.ViewDistanceScale.SecondaryScale 100` in the editor  to disable LOD optimizations so it renders all actors everything.
+
+
+   ![Minimap tool](https://i.imgur.com/1BvNDQp.png "Minimap tool")
+
+3. Create a master folder where you will keep all squad minimap generation folders (eg. SquadMinimaps). Create a folder __with the layer name__ (eg. Sumari_AAS_v1) within that master folder and export images from the minimap tool to that folder.
+
+4. Copy `BP_FlagExport.uasset` into `{SquadEditor}\Squad\Content\Blueprints\Tools` (or any other folder) then inside the editor drag the copied blueprint into the viewport. In the world outliner select the added bp and under details fill out the necesary boxes. Use the same markers that you used when creating the minimap. Finaly, click on the generate json button.
+
+
+   ![Settings](https://i.imgur.com/a2uSilC.png)
+
+5. Copy json from `{SquadEditor}\Squad` directory into the directory you extracted your images to. Your folder should look something like this.
+
+   ![Folder](https://i.imgur.com/jLvhANm.png)
+
+6. Open `stitcher.py` and inside change the input/output settings (line 10 - 29). Run the script. If you did everything correctly there will be a generated image (formated as {layer}_stitched.jpg) in the same folder `IMG_DIR` folder.
 
 ---
 
@@ -17,27 +50,18 @@ TODO
 
 Major
 ##
-* Fix flag zone rendring
-* Find out why the minimap tool only renders actors in the middle of the map and fix it
-* Better json exporting - printing in console now
-* Map border export + rendering
-* Support more gamemodes (Only AAS tested)
+* ~~Fix flag zone rendring~~
+* ~~Find out why the minimap tool only renders actors in the middle of the map and fix it~~ Fixed with `r.ViewDistanceScale.SecondaryScale 100`
+* ~~Better json exporting - printing in console now~~
+* ~~Map border export + rendering~~
+* __Support more gamemodes (Only AAS tested)__
 
 Minor
 ##
+* GUI tool
+* Multithreading
 * Test non square maps (probably all minimaps are square but not sure)
-* Custom minimap rendering bluprint
-   * Connect both blueprints into a single tool
+* ~~Custom minimap rendering bluprint~~ Found fix for stock
+   * ~~Connect both blueprints into a single tool~~
 * Fully automate everything
 * Finish documentation
----
-Workflow
-1. Open up SDK, and open up a layer (_don't open master layer unless you know how to add lighting_) you want to render (_first time it will take a very long time_).
-
-2. Open world properties and find minimap generation tool. Ingame minimaps have a resolution 4096 with a tile of 1. Increasing tiles will increase final resolution, however it might use up too much ram and crash if you choose a too large of a resolution-tile combo.
-![Minimap tool](https://i.imgur.com/1BvNDQp.png "Minimap tool")
-
-3. Create a folder with the layer name and extract images from the minimap tool to that folder
-
-4. Not finished WIP
-
--- stitcher.py
+++ stitcher.py
@@ -4,16 +4,39 @@ import math
 import json
 
 #pip install pillow
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageChops
 
-IMG_DIR = 'C:\\Users\\Lenart\\Documents\\MEGA\\Code\\SquadMinimaps'
-FLAG_DIR = 'C:\\Users\\Lenart\\Documents\\MEGA\\Code\\SquadMinimaps\\flags'
+
+# Settings
+
+# Folder names can be relative to the script execution location or absolute
+
+# Name of the layer folder located inside the IMG_DIR
+# master folder which contains tiled images and flag json
 MAP_LAYER = 'Sumari_AAS_v1'
+# Same number as when creating the minimap inside the editor
 TILES = 4
+# Resolution of the tile not image
 RES = 4096
 DRAW_FLAG = True
-TEAM_1 = 'gb'
-TEAM_2 = 'insurgents'
+DRAW_CAPTURE_ZONE = True
+DRAW_BORDER = True
+
+
+# Master folder which contains layer folders with images of layers and flag json
+IMG_DIR = ''
+# Folder which contains flag images and roboto default font
+RESOURCES_DIR = 'resources'
+
+# Used to translate faction name to its flag image file name
+faction_to_image = {
+    'GB' : 'gb',
+    'INS' : 'insurgents',
+    'MIL' : 'militia',
+    'RUS' : 'russia',
+    'USA' : 'usa'
+}
+
 
 # Used to translate world space coordinates to image coordinates
 def make_wti_interpolater(in_min, in_max, out_min, out_max): 
@@ -27,41 +50,35 @@ def make_wti_interpolater(in_min, in_max, out_min, out_max):
 
     return interp_fn
 
-
+# Function to draw text so that x,y is in the middle of the draw text
 def centered_text(draw, font, text, x, y):
     size = draw.textsize(text, font, stroke_width=5)
     n_x = x - size[0]/2
     n_y = y - size[1]/2
-    draw.text((n_x, n_y), font=font, text=text, fill=(255, 255, 255), stroke_fill=(0, 0, 0), stroke_width=5)
+    draw.text((n_x + 3, n_y + 3), font=font, text=text, fill=(0, 0, 0), stroke_fill=(0, 0, 0), stroke_width=1)
+    draw.text((n_x, n_y), font=font, text=text, fill=(255, 255, 255))
 
 def main():
+    # Create target image
     minimap = Image.new('RGBA', (TILES * RES, TILES * RES), color=(255, 255, 255))
 
+    # Stitch image
     for x in range(TILES):
         for y in range(TILES):
-            path = f'{IMG_DIR}\\{MAP_LAYER}\\{MAP_LAYER}_Minimap-{TILES-x-1}_{TILES-y-1}.TGA'
-            print(path)
+            file_name = f'{MAP_LAYER}_Minimap-{TILES-x-1}_{TILES-y-1}.TGA'
+            print(f'Stitching {file_name}')
+            path = f'{IMG_DIR}\\{MAP_LAYER}\\{file_name}'
             tile = Image.open(path)
 
             minimap.paste(tile, [RES * x, RES * y])
 
 
-    if DRAW_FLAG:
-        font = ImageFont.truetype('arial.ttf', size=72)
-
-        team_flags = [
-            Image.open(f'{FLAG_DIR}\\neutral_flag.TGA'),
-            Image.open(f'{FLAG_DIR}\\{TEAM_1}_flag.TGA'),
-            Image.open(f'{FLAG_DIR}\\{TEAM_2}_flag.TGA')
-        ]
-        for i in range(3):
-            team_flags[i] = team_flags[i].resize(
-                [s * 3 for s in team_flags[i].size]
-            )
-
+    if DRAW_FLAG or DRAW_CAPTURE_ZONE or DRAW_BORDER:
+        # Load flag data json
         with open(f'{IMG_DIR}\\{MAP_LAYER}\\{MAP_LAYER}_flags.json', 'r') as fh:
             flag_data = json.load(fh)
 
+        # Make world to picture coordinates interpolaters
         wti_itp_x = make_wti_interpolater(
             flag_data['corner_1']['x'],
             flag_data['corner_2']['x'],
@@ -76,60 +93,152 @@ def main():
             RES * TILES
         )
 
-
-        draw = ImageDraw.Draw(minimap)
-
-        for flag_id, flag in flag_data['flags'].items():
+    if DRAW_CAPTURE_ZONE:
+        zones_overlay = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 0))
+        zones_draw = ImageDraw.Draw(zones_overlay)
+        
+        for _, flag in flag_data['flags'].items():
+            print(f'Draw zone for {flag["display_name"]}')
             for obj_name, bbox in flag['bounds'].items():
+                # Draw a spherical bounding box (circle)
                 if 'sphere' in obj_name.lower():
                     x = wti_itp_x(bbox['origin']['x'])
                     y = wti_itp_y(bbox['origin']['y'])
-                    r = wti_itp_x(flag_data['corner_1']['x'] + bbox['box_extent']['x'])
-                    draw.arc(
+                    r = wti_itp_x(flag_data['corner_1']['x'] + bbox['radius'])
+                    zones_draw.ellipse(
                         [x - r/2, y - r/2
                         ,x + r/2, y + r/2],
-                        0, 360,
-                        fill=(0, 0, 255),
-                        width=20
+                        fill=(0, 0, 255, 255),
                     )
+                # Draw a square bounding box (rectangle)
                 elif 'box' in obj_name.lower():
                     x = wti_itp_x(bbox['origin']['x'])
                     y = wti_itp_y(bbox['origin']['y'])
                     b_x = wti_itp_x(flag_data['corner_1']['x'] + bbox['box_extent']['x'])
                     b_y = wti_itp_y(flag_data['corner_1']['y'] + bbox['box_extent']['y'])
-                    r = math.radians(bbox['rotation'])
+                    r = 180 - bbox['rotation']
 
-                    draw.rectangle(
-                        [x - b_x + math.cos(r) * b_x, y - b_y + math.sin(r) * b_y
-                        ,x + b_x + math.cos(r) * b_x, y + b_y + math.cos(r) * b_y],
-                        outline=(0, 0, 255),
-                        width=10
+                    rectangle = Image.new('RGBA', (int(b_x) * 2, int(b_y) * 2), color=(0, 0, 255, 255) )
+                    rectangle = rectangle.rotate(r, expand=True, fillcolor=(0, 0, 0, 0))
+
+                    zones_overlay.alpha_composite(
+                        rectangle,
+                        (int(x - rectangle.size[0]/2),
+                         int(y - rectangle.size[1]/2))
                     )
 
+        print('Compositing zones to image')
+        transparency = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 196))
+        zones_overlay = ImageChops.subtract(zones_overlay, transparency)
+        minimap.alpha_composite(zones_overlay)
 
+    if DRAW_FLAG:
+        # Create font object
+        font = ImageFont.truetype(f'{RESOURCES_DIR}\\Roboto-Regular.ttf', size=72)
+
+        # Load team flags
+        team_flags = [
+            Image.open(f'{RESOURCES_DIR}\\neutral_flag.TGA'),
+            Image.open(f'{RESOURCES_DIR}\\{ faction_to_image[flag_data["team_one"]] }_flag.TGA'),
+            Image.open(f'{RESOURCES_DIR}\\{ faction_to_image[flag_data["team_two"]] }_flag.TGA')
+        ]
+
+        # Upscale flags and make them translucent
+        for i in range(3):
+            team_flags[i] = team_flags[i].resize(
+                [s * 3 for s in team_flags[i].size]
+            )
+
+
+        # Create drawing object
+        objectives_overlay = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 0))
+        draw = ImageDraw.Draw(objectives_overlay)
+
+        # Draw a line connecting objectives
+        for _, flag in flag_data['flags'].items():
             if flag['next_flag']:
+                print(f'Draw objective line for {flag["display_name"]}')
                 x = wti_itp_x(flag['location']['x'])
                 y = wti_itp_y(flag['location']['y'])
                 next_flag_loc = flag_data['flags'][flag['next_flag']]['location']
                 n_x = wti_itp_x(next_flag_loc['x'])
                 n_y = wti_itp_y(next_flag_loc['y'])
-                print('draw line')
-                draw.line( [x, y, n_x, n_y], fill=(128, 128, 128), width=20)
+                draw.line( [x, y, n_x, n_y], fill=(255, 255, 255, 169), width=15)
 
-        for flag_id, flag in flag_data['flags'].items():
+        # Draw flags and name
+        for _, flag in flag_data['flags'].items():
+            print(f'Drawing flag for {flag["display_name"]}')
             x = wti_itp_x(flag['location']['x'])
             y = wti_itp_y(flag['location']['y'])
-            print('new flag')
-            minimap.alpha_composite(team_flags[int(flag['initial_team'])],
+            objectives_overlay.alpha_composite(
+                team_flags[flag['initial_team']],
+
                 (int(x - team_flags[int(flag['initial_team'])].size[0]/2),
                  int(y - team_flags[int(flag['initial_team'])].size[1]/2))
             )
-            centered_text(draw, font, flag['display_name'], x, y)
+            # Draw objective name
+            centered_text(draw, font, flag['display_name'], x, y - 150)
 
+        print('Compositing objectives to image')
 
+        transparency = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 55))
+        objectives_overlay = ImageChops.subtract(objectives_overlay, transparency)
+        minimap.alpha_composite(objectives_overlay)     
+
+    if DRAW_BORDER:
+        print('Parsing border line')
+        dict_border = {}
+        start = None
+
+        # Store unordered line segmets as a hash table where key is start and value is end
+        for segment in flag_data['border']:
+            s_x = wti_itp_x(segment['start']['x'])
+            s_y = wti_itp_y(segment['start']['y'])
+            e_x = wti_itp_x(segment['end']['x'])
+            e_y = wti_itp_y(segment['end']['y'])
+            if not start:
+                start = (s_x, s_y)
+            dict_border[ (s_x, s_y) ] = ( e_x, e_y )
+
+        # Store line segments in order
+        in_order_border = []
+        current_point = start
+        while True:
+            next_point = dict_border[current_point]
+            in_order_border += [p for p in current_point] + [p for p in next_point]
+
+            if next_point == start:
+                break
+            else:
+                current_point = next_point
+
+        print('Creating border')
+        # Create border mask similar to the one found in squad
+        blend_mask = Image.new('RGBA', (TILES * RES, TILES * RES), color=(255, 255, 255, 255))
+        bm_draw = ImageDraw.Draw(blend_mask)
+        bm_draw.polygon(in_order_border, fill=(0, 0, 0, 0))
+
+        # Blend outside of the border
+        border_outside = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 128))
+        bo_draw = ImageDraw.Draw(border_outside)
+        bo_draw.line(in_order_border, fill=(0, 0, 0, 255), width=100)
+        border_outside = border_outside.filter(ImageFilter.BoxBlur(100))
+
+        # Blend inside of the border
+        border_inside = Image.new('RGBA', (TILES * RES, TILES * RES), color=(0, 0, 0, 0))
+        bi_draw = ImageDraw.Draw(border_inside)
+        bi_draw.polygon(in_order_border, fill=(0, 0, 0, 0))
+        bi_draw.line(in_order_border, fill=(0, 0, 0, 128), width=50)
+        border_inside = border_inside.filter(ImageFilter.BoxBlur(50))
+        
+        print('Creating border composite')
+        border = ImageChops.composite(border_outside, border_inside, blend_mask)
+        print('Compositing border to image')
+        minimap.alpha_composite(border)
+
+    print('Saving minimap')
     minimap = minimap.convert('RGB')
     minimap.save(f'{IMG_DIR}\\{MAP_LAYER}_stitched.jpg', 'JPEG', quality=90)
 
 if __name__ == "__main__":
     main()
-
